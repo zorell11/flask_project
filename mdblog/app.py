7ff554fc78cb609e4fa35cc35758739c33583e9d
zorell11 <zorell@azet.sk>
2021-05-09 18:22:25 +0200

    using jinja template system

--- mdblog/app.py
+++ mdblog/app.py
@@ -1,23 +1,31 @@
 from flask import Flask
 from flask import render_template
+from .database import articles
 
 flask_app = Flask(__name__)
 
 @flask_app.route('/')
-def welcome_page():
+def view_welcome_page():
     return render_template("welcome_page.jinja")
 
 
 @flask_app.route('/about')
-def about():
+def view_about():
     return render_template("about.jinja")
 
 
 @flask_app.route('/admin')
-def admin():
+def view_admin():
     return render_template("admin.jinja")
 
 
 @flask_app.route('/articles')
-def articles():
-    return render_template("articles.jinja")
+def view_articles():
+    return render_template("articles.jinja", articles=articles)
+
+@flask_app.route('/article/<int:art_id>')
+def view_article(art_id):
+    article = articles.get(art_id)
+    if article:
+        return render_template("article.jinja", article=article)
+    return render_template("article_not_found.jinja", art_id=art_id)
---
+++ mdblog/database.py
@@ -0,0 +1,18 @@
+articles = {
+            1 : {
+                "title" : "Hello World1",
+                "content" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+                },
+            2 : {
+                "title" : "Hello World2",
+                "content" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, mel tollit detraxit ex. Mundi homero sed no, pro te stet agam gubergren, agam facilisis his ne. Ea mea praesent signiferumque. Id est congue nominavi necessitatibus, id augue nostrud euripidis mea, ea eos inani accumsan vulputate. Eu sea utamur oportere, mei odio invenire ut. Nam et graece ullamcorper, duo in dolor legere putent, est id inani concludaturque."
+                },
+            3 : {
+                "title" : "Hello World3",
+                "content" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+                },
+            4 : {
+                "title" : "Hello World4",
+                "content" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, mel tollit detraxit ex. Mundi homero sed no, pro te stet agam gubergren, agam facilisis his ne. Ea mea praesent signiferumque. Id est congue nominavi necessitatibus, id augue nostrud euripidis mea, ea eos inani accumsan vulputate. Eu sea utamur oportere, mei odio invenire ut. Nam et graece ullamcorper, duo in dolor legere putent, est id inani concludaturque."
+                }
+        }
